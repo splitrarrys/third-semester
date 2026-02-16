4fdb6d8c03574f3c4e0f1bc9492729f1a13e5881
panychev_dima <splitrarrys@gmail.com>
2026-02-16 11:40:54 +0300

    Update person.h
    a

--- task2/person.h
+++ task2/person.h
@@ -6,17 +6,20 @@
 
 class Person {
 protected:
+    int id;
     std::string name;
     int age;
+    static int nextId;
 
 public:
-    Person() = default;
+    Person();
     Person(const std::string& name, int age);
     virtual ~Person() = default;
 
     virtual void printInfo() const;
     virtual void readFromInput();
 
+    int getId() const;
     std::string getName() const;
     int getAge() const;
 
